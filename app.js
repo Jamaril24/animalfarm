60251bffde4ac13434d534a3ad95e6088b675601
Jamaril24 <jamarlopez24@g.crookcountyschools.org>
2022-03-31 10:07:16 -0700

    day three

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ catButton.addEventListener('click', function(){
   // TODO: assign the list item's text to be 'Cat' or the name of a cat you know.
   newCat.textContent = 'cat';
   const cats = document.getElementById('cats');
-
   cats.appendChild(newCat);
 });
 
@@ -22,23 +21,31 @@ const dogButton = document.getElementById('make-dog');
 dogButton.addEventListener('click', function () {
   const newDog = document.createElement('li');
   newDog.textContent = 'dog';
- const dogs = document.createElementById('dog');
+ const dogs = document.getElementById('dogs');
   dogs.appendChild(newDog);
 });
 
 const sheepButton = document.getElementById('make-sheep');
 
 // TODO: add an event listener to the sheepButton that calls `handleMakingSheep` when clicked
-sheepButton.addEventListener('click', function()){
-
-}
+sheepButton.addEventListener('click', handleMakingSheep);
 
 function handleMakingSheep(){
-
-  // TODO: make a new list item, assign its text content to `Sheep` or the name of a sheep you know, and append it to the third unordered list.
-newSheep.textContent = 'sheep'
+// TODO: make a new list item, assign its text content to `Sheep` or the name of a sheep you know, and append it to the third unordered list.
+const newSheep = document.createElement('li');
+newSheep.textContent = 'sheep';
+const sheeps = document.getElementById('sheeps');
+sheeps.appendChild(newSheep);
 }
 
 // TODO: create an event listener for the `Make Horse` Button that calls a function named `handleMakingHorse`
+const horsebutton = document.getElementById('make-horse');
+horsebutton.addEventListener('click', handleMakinghorse);
 
 // TODO: write a function called `handleMakingHorse that makes a new list item, assigns its text content to `Horse` or the name of a horse you know, and append it to the fourth unordered list.
+function handleMakinghorse(){
+  const newHorse = document.createElement('li');
+  newHorse.textContent = 'Horse';
+  const horse = document.getElementById('horses');
+  horsebutton.appendChild(newHorse);
+}
